7b9d9120d3fd0de6d2fe658016bc778f6f24a3d2
mehdilagdimi <lagdimi.mehdi@gmail.com>
2023-01-27 16:48:40 +0100

    tyrin to fix cors policy err

--- Frontend/BankFrontend/src/app/interceptors/app-http.interceptor.ts
+++ Frontend/BankFrontend/src/app/interceptors/app-http.interceptor.ts
@@ -21,15 +21,14 @@ export class AppHttpInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
-    const token = this.authStorageService.get("myrh-token");
+    const token = this.authStorageService.get("bankconnect-token");
     this.jwtService.setToken(token!);
 
-    //check if requesting another domain other than localhost so we don't include Authorization header
-    if(request.url.includes("parseapi.back4app.com")) return next.handle(request);
+
 
     if (token) {
       if(this.jwtService.isTokenExpired()) {
-        this.authStorageService.remove("myrh-token");
+        this.authStorageService.remove("bankconnect-token");
         return next.handle(request);
       }
 
