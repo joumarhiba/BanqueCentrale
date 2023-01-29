162be1d58c12a1b025d754d43e356265cbfd9898
mehdilagdimi <lagdimi.mehdi@gmail.com>
2023-01-29 13:45:43 +0100

    lookaround for cors

--- Frontend/BankFrontend/src/config/api.constants.ts
+++ Frontend/BankFrontend/src/config/api.constants.ts
@@ -1,3 +1,2 @@
-// export const API_URL = "https://bankconnect-api.herokuapp.com";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://bankconnect-api.herokuapp.com";
 
